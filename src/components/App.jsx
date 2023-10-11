2b0f876074e9054a4808e9941599d081c3dc2a05
Serhii120796 <sfedyura@ukr.net>
2023-10-11 22:57:20 +0300

    Initial settings

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,8 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Layout } from './Layout';
+
+export class App extends Component {
+  render() {
+    return <Layout></Layout>;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'components/GlobalStyle';
+
+const theme = {
+  radius: '5px',
+  spasing: value => `${value * 4}px`,
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+      <GlobalStyle/>
+    </ThemeProvider>
   </React.StrictMode>
 );
